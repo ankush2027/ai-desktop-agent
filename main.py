b5af241bddc482924a5bb31be91d5e9d272ed55f
Ankush <ankushgowdaa10@gmail.com>
2026-05-05 22:32:48 +0530

    feat: add command-based site opening system

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from actions.browser import open_site
 
-open_site("youtube")
-open_site("google")
+
+def main():
+    command = input("Enter command: ").lower().strip()
+
+    if not command:
+        print("Empty command")
+        return
+
+    parts = command.split()
+
+    if parts[0] == "open":
+        if len(parts) > 1:
+            open_site(parts[1])
+        else:
+            print("Please specify what to open (e.g., 'open youtube').")
+    else:
+        print("Command not recognized.")
+
+
+if __name__ == "__main__":
+    main()
