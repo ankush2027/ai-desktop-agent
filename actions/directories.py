5f73e2528aa59f208d8b74796a03ad57dc06a2a2
Ankush <ankushgowdaa10@gmail.com>
2026-07-09 16:34:45 +0530

    fix: implement folder creation

--- actions/directories.py
+++ actions/directories.py
@@ -1,4 +1,8 @@
 import os
 
 def create_folder(folder_name):
-    ...
+    try:
+        os.makedirs(folder_name)
+        print(f"Created '{folder_name}'.")
+    except FileExistsError:
+        print(f"'{folder_name}' already exists.")
