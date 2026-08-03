8ab67d2b008d868833682d03dd87d3fd61bc93c3
Ankush <ankushgowdaa10@gmail.com>
2026-08-03 19:51:26 +0530

    refactor: improve logging system

--- logger.py
+++ logger.py
@@ -1,2 +1,21 @@
+import os
+from datetime import datetime
+
+
+LOG_FOLDER = "logs"
+LOG_FILE = os.path.join(LOG_FOLDER, "history.log")
+
+
 def log_action(action, target):
+    if not os.path.exists(LOG_FOLDER):
+        os.makedirs(LOG_FOLDER)
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     print(f"[LOG] {action} --> {target}")
+
+    with open(LOG_FILE, "a") as file:
+        file.write(f"Time   : {current_time}\n")
+        file.write(f"Action : {action}\n")
+        file.write(f"Target : {target}\n")
+        file.write("-" * 40 + "\n")
